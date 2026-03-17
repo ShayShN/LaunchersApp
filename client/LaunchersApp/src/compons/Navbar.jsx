a11c47c15b15cb1a45788043a3d149462f6e12f4
shay <shayshn1234@gmail.com>
2026-03-17 18:24:29 +0200

    build register and more

--- client/LaunchersApp/src/compons/Navbar.jsx
+++ client/LaunchersApp/src/compons/Navbar.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
+  const navigate = useNavigate()
+
+  const token = localStorage.getItem("token")
+  const currentUser = localStorage.getItem("found")
+  const userParse = JSON.parse(currentUser)
+  
+  
+  
+  
+
+  function handleLogout() {
+    localStorage.removeItem("token")
+    localStorage.removeItem("found")
+    navigate("/login")
+    window.location.reload()
+    
+  }
+
+  function handleActiveUser() {
+    if (!token) {
+      navigate("/login")
+    }
+    alert(`type: ${userParse.user_type}, username: ${userParse.username}`)
+  }
+
   return (
-    <div>Navbar</div>
+    <nav className='flex p-3 gap-3'>
+      <Link className='border w-fit' to={"/login"}>Login Page</Link>
+      {token && <button className='border w-fit cursor-pointer' onClick={handleLogout}>Logout</button>}
+      {userParse?.user_type == "admin" && <Link className='border w-fit' to={"/getUser"}>Register Page</Link>}
+      <button className='border w-fit cursor-pointer' onClick={handleActiveUser}>Get Active User</button>
+    </nav>
   )
 }
--- client/LaunchersApp/src/pages/LoginPage.jsx
+++ client/LaunchersApp/src/pages/LoginPage.jsx
@@ -1,12 +1,46 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+
 export default function LoginPage() {
     const navigate = useNavigate()
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        fetchData()
+    }
+    async function fetchData() {
+        try {
+            const response = await fetch("http://localhost:3001/api/auth/login",{
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+              username: username,
+              password: password
+            })
+            })
+            const data = await response.json()
+            localStorage.setItem("token", data.token)
+            localStorage.setItem("found", JSON.stringify(data.found))
+            navigate("/")
+            window.location.reload()
+        } catch (error) {
+            alert("loggin faild");
+            
+        }
+    }
+
     return (
-    <div>LoginPage</div>
-  )
+        
+        <div onSubmit={handleSubmit}>
+            <form className='flex gap-3' >
+                <input className='border p-2' type="text" value={username} placeholder='username' onChange={(e) => setUsername(e.target.value)} />
+                <input className='border p-2' type="password" value={password} placeholder='password' onChange={(e) => setPassword(e.target.value)} />
+                <button className='border w-fit cursor-pointer p-2 hover:bg-amber-400' type="submit">Send</button>
+            </form>
+        </div>
+    )
 }
